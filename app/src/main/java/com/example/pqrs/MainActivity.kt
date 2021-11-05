981c2152fd0297905fadc3b15665364f05b5541c
Eric jose navarro german <enavarro.extreme@gmail.com>
2021-11-04 22:04:00 -0500

    ni ideas

--- app/src/main/java/com/example/pqrs/MainActivity.kt
+++ app/src/main/java/com/example/pqrs/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pqrs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
